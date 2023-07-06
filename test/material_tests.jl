d7f487e83d8807ed383634330d22d385db5fb7b1
Carson Farmer <59753859+cfarm6@users.noreply.github.com>
2023-07-06 13:04:41 -0400

    Added CodeCov Badge

--- test/material_tests.jl
+++ test/material_tests.jl
@@ -1,6 +1,13 @@
 @testset "Material Tests" begin
     @test HyperelasticUniaxialTest(ones(3), ones(3), name="test") isa HyperelasticUniaxialTest
+    @test HyperelasticUniaxialTest(ones(3), ones(3), incompressible=false,name="test") isa HyperelasticUniaxialTest
+
     @test HyperelasticUniaxialTest(ones(3), name="test") isa HyperelasticUniaxialTest
+    @test HyperelasticUniaxialTest(ones(3), incompressible=false, name="test") isa HyperelasticUniaxialTest
+
     @test HyperelasticBiaxialTest(ones(3), ones(3), ones(3), ones(3), name="test") isa HyperelasticBiaxialTest
-    @test HyperelasticBiaxialTest(ones(3), ones(3), name="test") isa HyperelasticBiaxialTest
+    @test HyperelasticBiaxialTest(ones(3), ones(3), ones(3), ones(3), name="test", incompressible=false) isa HyperelasticBiaxialTest
+
+    @test HyperelasticBiaxialTest(ones(3), ones(3), name="test", ) isa HyperelasticBiaxialTest
+    @test HyperelasticBiaxialTest(ones(3), ones(3), name="test", incompressible=false) isa HyperelasticBiaxialTest
 end
